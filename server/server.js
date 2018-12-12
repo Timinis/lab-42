904aa580b37fe47548f81a6170665e5ba6c2cf1f
Timinis <litimc3@gmail.com>
2018-12-11 18:53:32 -0800

    added server code

--- server/server.js
+++ server/server.js
@@ -9,15 +9,10 @@ const PORT = process.env.PORT || 3000;
 const io = require('socket.io')(PORT);
 
 io.on('connection', socket => {
-
   console.log('Connected', socket.id);
 
-  socket.on('troll', (payload) => {
+  socket.on('troll', payload => {
     console.log('broadcast', payload);
-    socket.broadcast.emit('incoming', payload);
+    io.emit('incoming', payload);
   });
-
 });
-
-
-
